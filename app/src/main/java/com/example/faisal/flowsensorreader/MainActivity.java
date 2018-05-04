85e52e2c8c7e306d48a624bfc2f406c8489b3582
Faisal <faisal@Faisals-MacBook-Pro-2.local>
2018-05-04 03:11:25 +0300

    created documentation

--- app/src/main/java/com/example/faisal/flowsensorreader/MainActivity.java
+++ app/src/main/java/com/example/faisal/flowsensorreader/MainActivity.java
@@ -36,58 +36,75 @@ public class MainActivity extends AppCompatActivity {
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // super teelling the andoid to access this page.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+/// final = means theat does not change
         final Activity mainActivity = this;
+        //this means refer to the main activity
         //Get access to contact button
+        // contact button emaill
+        /// refuer to the id which contact button to the activity main xml
+
         Button contactButton = (Button) findViewById(R.id.contactButton);
+        // when you clik, it should runs
 
         //Listen for the button to be clicked and run any code inside of the function onClick(View v).
         contactButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                // Create intent.
-
+                // the subject contact flow sensor
                 String subject = "Contact Flow Sensor";    // subject for email
-                String address = "contact@flow.com";      // email address for the company
+                String address = "zkohl@rubiconsensors.com";      // email address for the company
 
                 ShareCompat.IntentBuilder.from(mainActivity)
                         .setType("message/rfc822")
                         .addEmailTo(address)
                         .setSubject(subject)
-                        .setChooserTitle("Select mail client")
+                        .setChooserTitle("Select mail client")///option which email
                         .startChooser();
             }
         });
 
+        // The only reason the 'final' keyword is here is because it is being
+        // accessed from a private class. The private class is new View.OnClicListener()...
         final EditText usernameText = (EditText) findViewById(R.id.usernameInput);
+        //EditText is the type that it takes from activity_mail.xml
         final EditText passwordText = (EditText) findViewById(R.id.passwordInput);
+        //EditText is the type that it takes from activity_mail.xml
+
         Button loginButton = (Button) findViewById(R.id.loginButton);
+        //Button is the type that it takes from activity_mail.xml
 
 
         final Intent homePageIntent = new Intent (this, HomeActivity.class);
 
 
+        //When the user presses the 'loginButton' it runs this code.
         loginButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                //access to the text inside of 'usernameText' and 'passwordText'
                 String username = usernameText.getText().toString();
                 String password = passwordText.getText().toString();
 
+                // Check if the username and password is empty
                 if(username.length() == 0 || password.length() == 0){
+                    //If the username and password is empty, tell the user that something is wrong.
                     AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-                    alertDialog.setTitle("Invalid Username or Password");
-                    alertDialog.setMessage("Your username and password must not be empty.");
+                    alertDialog.setTitle("Invalid Username or Password"); // The title of the alert
+                    alertDialog.setMessage("Your username and password must not be empty."); //message of the alert
                     alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
                             new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialogInterface, int i) {
-                                    dialogInterface.dismiss();
+                                    dialogInterface.dismiss(); // when you press 'ok' it removes the alert
                                 }
                             });
-                    alertDialog.show();
+                    alertDialog.show(); // show the alert.
                 } else {
+                    // the user and password is correct,
+                    // change the activity to the homepage (Sends the user to the homepage)
                     startActivity(homePageIntent);
                 }
             }
--- app/src/main/java/com/example/faisal/flowsensorreader/MyPage.java
+++ app/src/main/java/com/example/faisal/flowsensorreader/MyPage.java
@@ -28,48 +28,54 @@ import java.util.List;
 import java.util.Map;
 
 public class MyPage extends AppCompatActivity {
-
+    // dateFormat tells the computer what format the JSON date will be in.
+    // for example 2018-04-12T14:42:42.230Z
     final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Tell android to show activity_my_page.xml
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
 
-        // Reference the chart Container
+        // Reference the chart Container refers to the id 'chart_parent'
         ViewGroup chartContainer = (ViewGroup) findViewById(R.id.chart_parent);
 
-        LineChartStyle lineChartStyle = new LineChartStyle();
-        lineChartStyle.setDrawPointCenter(false);
-        LineChartStyle.Border leftBottomBorder = new LineChartStyle.Border(
-                LineChartStyle.Border.LEFT, LineChartStyle.Border.BOTTOM
+        // Details the style for the line chart
+        // https://github.com/hogelog/line-chart-view/blob/master/line-chart-view-demo/src/main/java/org/hogel/android/linechartviewdemo/DateLineChartActivity.java
+        LineChartStyle lineChartStyle = new LineChartStyle(); // create new LineChartStyle object
+        lineChartStyle.setDrawPointCenter(false); // have no center point at origin
+        LineChartStyle.Border leftBottomBorder = new LineChartStyle.Border( // describe the border.
+                LineChartStyle.Border.LEFT, LineChartStyle.Border.BOTTOM // the border is at the left and the bottom only.
         );
-        leftBottomBorder.setWidth(8.0f);
-        lineChartStyle.addBorder(leftBottomBorder);
+        leftBottomBorder.setWidth(8.0f); // set the width of the lines 'f' means the data type of 8 is a float
+        lineChartStyle.addBorder(leftBottomBorder); // applies the border
         lineChartStyle.setXLabelFormatter(new LineChartStyle.LabelFormatter() {
             @Override
             public String format(long value) {
-                return DateFormat.format("MM/dd", value).toString();
+                return DateFormat.format("MM/dd", value).toString(); // apply the date format for the line chart
             }
         });
 
-        lineChartStyle.setYLabelWidth(80.0f);
+        // 80 pixels between the graph and the edge of the phone
+        lineChartStyle.setYLabelWidth(80.0f); // set the y label width. how much space the y axis label has.
 
-        final DateLineChartView chartView = new DateLineChartView(this, lineChartStyle);
-        chartView.setStyle(lineChartStyle);
-        chartContainer.addView(chartView);
+        final DateLineChartView chartView = new DateLineChartView(this, lineChartStyle); // create our line chart and apply the style
+        chartView.setStyle(lineChartStyle); // set style again. you must
+        chartContainer.addView(chartView); // insert the line chart into the activity 'activity_my_page.xml'
 
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(MyPage.this);
+        // Create the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(MyPage.this); //this is how it is
 
-        String url = "https://riversense.rubiconsensors.com/api";
+        String url = "https://riversense.rubiconsensors.com/api"; // url where to get the data
 
         // Request a string response from the provided URL.
+        // new creates an object
+        // get is the type of requests
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        System.out.println("RESPONSE: " + response);
                         try{
                             // Save response as a JSONObject
                             JSONObject root  = new JSONObject(response);
@@ -80,24 +86,28 @@ public class MyPage extends AppCompatActivity {
                             // Loop through all the keys
                             while(iter.hasNext()){
                                 // Get key name (Date)
+                                // key-> "2018-04-12T14:42:42.230Z": value->0.0
                                 String key = (String)iter.next();
+
                                 // Get value name (sensor data)
                                 long value = root.getLong(key);
 
                                 // Add to list of points (may not parse correctly)
                                 try {
+                                    //check if the date is greater than a week ago
                                     Calendar c=Calendar.getInstance();
-                                    c.add(Calendar.DATE,-7);
+                                    c.add(Calendar.DATE,-7); // Current date - 7 = 7 days ago
                                     if(c.getTime().compareTo(dateFormat.parse(key)) <= 0){
+                                        // if the date is less than a week, then add to list of points
                                         points.add(new LineChartView.Point(date(key), value));
                                     }
 
                                 } catch (ParseException e) {
-                                    throw new RuntimeException(e);
+                                    throw new RuntimeException(e); // if there is an error, stop the program
                                 }
                             }
 
-                            // Set the points
+                            // add the points to the line chart
                             chartView.setPoints(points);
                         } catch(Exception ex){
                             System.out.println(ex.getMessage());
@@ -111,6 +121,7 @@ public class MyPage extends AppCompatActivity {
             }
         });
 
+        // make the request
         queue.add(stringRequest);
         System.out.println("adding to request queue");
     }
